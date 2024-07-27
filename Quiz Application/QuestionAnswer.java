0ccc598af74dc3edb445749ddba2e2374d35cc19
ShivaKumarKalavari <134154341+ShivaKumarKalavari@users.noreply.github.com>
2024-07-27 11:26:30 +0530

    Create QuestionAnswer.java

---
+++ Quiz Application/QuestionAnswer.java
@@ -0,0 +1,24 @@
+package com.example.quiz;
+public class QuestionAnswer {
+    public static String question[] ={
+            "What is the full form of MAD?",
+            "Which one is not the terminology used in mobile application development?",
+            "Who owns the Android?",
+            "Which one of these not an android API?",
+            "Which application in android will allow to download and install different applications?"
+    };
+    public static String choices[][] = {
+            {"Mobile Adequate Development","Mobile Application Development","Middle Aged Dog","Moe Aeo Din"},
+            {"Views","Buttons","Intent","NumPy"},
+            {"Facebook","Microsoft","Google","Apple"},
+            {"Lollipop","Marshmallow","KitKat","Panda"},
+            {"I Store","Play Store","G-hub","G-store"}
+    };
+    public static String correctAnswers[] = {
+            "Mobile Application Development",
+            "NumPy",
+            "Google",
+            "Panda",
+            "Play Store"
+    };
+}
